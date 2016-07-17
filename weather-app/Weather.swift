31d772a4f33c1af0006baf8110737a333d2ce4a6
Adam Goth <adamgoth@gmail.com>
2016-07-17 09:11:16 -0600

    Adds alamofire pod, prompts user for location authorization

---
+++ weather-app/Weather.swift
@@ -0,0 +1,17 @@
+//
+//  Weather.swift
+//  weather-app
+//
+//  Created by Adam Goth on 7/17/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class Weather {
+    
+    //func downloadWeatherDetails(completed: DownloadComplete) {
+        
+    //}
+}
